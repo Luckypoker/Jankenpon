b8bb1b9cdae8712999b79c036a69d6c9f2f86815
Tyler <geniusatwork543@gmail.com>
2022-07-26 15:36:02 -0400

    Began writing out a single round

--- Duel.js
+++ Duel.js
@@ -9,3 +9,12 @@ else if (getCompChoice(3) === 2) {
 }
 else {console.log("Paper");
 }
+//This function should allow the player to input a selection and play a round with the computer
+//and out put a win or loss condition ex. You lose. Rock beats Scissors
+function playRound(playerSelection, computerSelection) {
+    let answer = parseInt(prompt("Time to duel! Choose Rock, Paper, or Scissors!"));
+    let computerSelection = getCompChoice;
+    if (playerSelection == "Rock") {
+        playerSelection == computerSelection
+    }
+}
