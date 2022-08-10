981b372a6135ae72e5d7ccfea3210502b43261a8
Artificient <geniusatwork543@gmail.com>
2022-08-09 23:22:08 -0400

    Updated lines to switch statements

--- Duel.js
+++ Duel.js
@@ -1,48 +1,63 @@
-function getCompChoice(max) {
-    var randomNumber = Math.floor(Math.random() * max);
-    if (randomNumber === 1) {
-     return "Rock";
+//Computer should randomly output either Rock, Paper, or Scissors
+let playerScore = 0;
+let compScore = 0;
+function getComputerChoice(max) {
+    var randomNumber = Math.floor(Math.random() * max);    
+    switch (randomNumber) {
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 0:
+            return "Scissors";
     }
-    else if (randomNumber === 2) {
-    return "Scissors";
     }
-    else {return "Paper";}
+     //Player and computer should be able to play out a round with the result being logged
+function playRound (playerSelection, computerSelection) {
+    switch (playerSelection) {
+        case "Rock":
+            switch (computerSelection) {
+                case "Rock":
+                    return result = "Tie! Just two rocks chilling in a hot tub~";
+                case "Paper":
+                    return result = "You lose! Paper beats rock, because throwing one at someone isn't as bad as throwing the other";
+                case "Scissors":
+                    return result = "You win! Rock beats scissors, good for the kid that discovered that but less so for the parents";
+            }
+        case "Paper":
+            switch (computerSelection) {
+                case "Rock":
+                    return result = "You win! Paper beats rock! Congrats writing beats the unga bunga";
+                case "Paper":
+                    return result = "Tie! Two treaties signed, but peace could never last...";
+                case "Scissors":
+                    return result = "You lose! Scissors beats paper! Don't blame yourself though, they were built specifically to stop you";
+            }
+        case "Scissors":
+            switch (computerSelection) {
+                case "Scissors":
+                    return result = "Tie! Scissor fights are fun for the whole family!";
+                case "Rock":
+                    return result = "You lose! Rock beats scissors. Honestly putting either one in a snowball would hurt";
+                case "Paper":
+                    return result = "You win! Scissors beats paper! All your base are belong to us";
+            }
+    }
 }
-getCompChoice(3);
-//This function should allow the player to input a selection and play a round with the computer
-//and out put a win or loss condition ex. You lose. Rock beats Scissors
-var playerSelection = window.prompt("Time to duel! Pick Rock, Paper, or Scissors");
-function playRound(playerSelection, computerSelection) {
-    if ((playerSelection === "Rock") && (computerSelection === "Rock")) {
-        return result = "Tie Just two rocks chilling in a hot tub~";
-    }   else if ((playerSelection === "Rock") && (computerSelection === "Paper")) {
-        return result = "You lose! Paper beats rock, because throwing one at someone isn't as bad as throwing the other";
-    }   else if ((playerSelection === "Rock") && (computerSelection === "Scissors")) {
-        return result = "You win! Rock beats scissors, good for the kid that discovered that but less so for the parents";
-    }   else if (playerSelection === "Paper" && computerSelection === "Paper") {
-        return result = "Tie Two treaties signed, but peace could never last..."
-    }   else if ((playerSelection === "Paper") && (computerSelection === "Rock")) {
-        return result = "You win! Paper beats rock! Congrats writing beats the unga bunga"
-    }   else if ((playerSelection === "Paper") && (computerSelection === "Scissors")) {
-        return result = "You lose! Scissors beats paper! Don't blame yourself though, they were built specifically to stop you"
-    }   else if ((playerSelection && computerSelection) === "Scissors") {
-        return result = "Tie Scissor fights are fun for both the kids and adults! Inuendo!"
-    }   else if ((playerSelection === "Scissors") && (computerSelection === "Rock")) {
-        return result = "You lose! Rock beats scissors. Honestly putting either one in a snowball would hurt"
-    }   else if ((playerSelection === "Scissors") && (computerSelection === "Paper")) {
-        return result = "You win! Scissors beats paper! All your base are belong to us"
-    }
-        else {
-        return playerSelection;
+//This plays out a set amount of rounds defined by #1
+function playGame (j) {
+    for (let i = 1; i <= j; i++) {
+        console.log(playRound(window.prompt("Time to duel! Choose Rock, Paper, or Scissors"), getComputerChoice(3)));
+        console.log(playerScore, compScore);
+        if (i == 5) {
+            if (playerScore > compScore) {
+            console.log("Congratulations! Winner winner chicken dinner!");
+            } else if (j === 5 && (playerScore < compScore));
         }
+    }
 }
-const computerSelection = getCompChoice(3);
-function playGame(i) {
-    for (let i = 1; i <= 5; i++) {
-        playerSelection;
-        console.log(playRound)
-        console.log(i); 
-    } if (i === 5) {console.log("Game Over");}
-    return;
-}
-playGame(5);
+playGame(5); //#1
+getComputerChoice(3);
+	
+	
+	
