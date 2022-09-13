f2382cd150846e739eb4a59d2b20a591abc969ae
Artificient <geniusatwork543@gmail.com>
2022-09-13 15:44:16 -0400

    Finished the Duel script to track and add to player and comp score and added three buttons on the index plate

--- Duel.js
+++ Duel.js
@@ -20,27 +20,27 @@ function playRound (playerSelection, computerSelection) {
                 case "Rock":
                     return result = "Tie! Just two rocks chilling in a hot tub~";
                 case "Paper":
-                    return result = "You lose! Paper beats rock, because throwing one at someone isn't as bad as throwing the other";
+                    return result = compWins(), "You lose! Paper beats rock, because throwing one at someone isn't as bad as throwing the other";
                 case "Scissors":
-                    return result = "You win! Rock beats scissors, good for the kid that discovered that but less so for the parents";
+                    return result = playerWins(), "You win! Rock beats scissors, good for the kid that discovered that but less so for the parents";
             }
         case "Paper":
             switch (computerSelection) {
                 case "Rock":
-                    return result = "You win! Paper beats rock! Congrats writing beats the unga bunga";
+                    return result = playerWins(), "You win! Paper beats rock! Congrats writing beats the unga bunga";
                 case "Paper":
                     return result = "Tie! Two treaties signed, but peace could never last...";
                 case "Scissors":
-                    return result = "You lose! Scissors beats paper! Don't blame yourself though, they were built specifically to stop you";
+                    return result = compWins(), "You lose! Scissors beats paper! Don't blame yourself though, they were built specifically to stop you";
             }
         case "Scissors":
             switch (computerSelection) {
                 case "Scissors":
                     return result = "Tie! Scissor fights are fun for the whole family!";
                 case "Rock":
-                    return result = "You lose! Rock beats scissors. Honestly putting either one in a snowball would hurt";
+                    return result = compWins(), "You lose! Rock beats scissors. Honestly putting either one in a snowball would hurt";
                 case "Paper":
-                    return result = "You win! Scissors beats paper! All your base are belong to us";
+                    return result = playerWins(), "You win! Scissors beats paper! All your base are belong to us";
             }
     }
 }
@@ -52,10 +52,18 @@ function playGame (j) {
         if (i == 5) {
             if (playerScore > compScore) {
             console.log("Congratulations! Winner winner chicken dinner!");
-            } else if (j === 5 && (playerScore < compScore));
+            } else if ((playerScore < compScore)) {
+                console.log("Imagine getting ratio'd by a program written by an alcoholic... couldn't be me")
+            } else {console.log("Holy smokes Lois, it's a tie!")};
         }
     }
 }
+function playerWins () { 
+    playerScore++
+};
+function compWins () {
+    compScore++
+};
 playGame(5); //#1
 getComputerChoice(3);
 	
