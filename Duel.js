3eadd9b8ac6ddddbe05e0219a8139000d4f3a8dc
Artificient <geniusatwork543@gmail.com>
2022-09-20 16:44:50 -0400

    Rewrote the way the system takes and sends information, IE completely rewrote the logs into DOM manipulations

--- Duel.js
+++ Duel.js
@@ -1,6 +1,47 @@
-//Computer should randomly output either Rock, Paper, or Scissors
 let playerScore = 0;
 let compScore = 0;
+//DOM Manipulators
+const container = document.querySelector('#container')
+const content = document.createElement("div");
+content.classList.add("content");
+container.appendChild(content)
+//Lists the scores
+const scores = document.createElement("h1")
+scores.classList.add("scores")
+scores.textContent = `Player: ${playerScore} Computer: ${compScore}`
+content.appendChild(scores)
+//Gameplay Buttons
+const btnr = document.querySelector("#btnr")
+btnr.addEventListener('click', () => {
+   results.textContent = playRound("Rock")
+   roundResult.appendChild(results)
+   scores.textContent = (`Player: ${playerScore} Computer: ${compScore}`)
+   content.appendChild(scores)
+   playGame()
+})
+const btnp = document.querySelector('#btnp')
+btnp.addEventListener('click', () => {
+    results.textContent = playRound("Paper")
+    roundResult.appendChild(results)
+    scores.textContent = (`Player: ${playerScore} Computer: ${compScore}`)
+    content.appendChild(scores)
+    playGame()
+})
+const btns = document.querySelector('#btns')
+btns.addEventListener('click', () => {
+    results.textContent = playRound("Scissors")
+    roundResult.appendChild(results)
+    scores.textContent = (`Player: ${playerScore} Computer: ${compScore}`)
+    content.appendChild(scores)
+    playGame()
+})
+
+const roundResult = document.createElement('div')
+roundResult.classList.add('roundResult')
+content.appendChild(roundResult)
+const results = document.createElement('p')
+results.classList.add("results")
+//Computer should randomly output either Rock, Paper, or Scissors
 let computerSelection = function getComputerChoice(max) {
     var randomNumber = Math.floor(Math.random() * max);    
     switch (randomNumber) {
@@ -13,7 +54,7 @@ let computerSelection = function getComputerChoice(max) {
     }
     }
      //Player and computer should be able to play out a round with the result being logged
-function playRound (playerSelection, com) {
+function playRound (playerSelection) {
     switch (playerSelection) {
         case "Rock":
             switch (computerSelection(3)) {
@@ -45,17 +86,28 @@ function playRound (playerSelection, com) {
     }
 }
 //This plays out a set amount of rounds defined by #1
-function playGame (j) {
-    for (let i = 1; i <= j; i++) {
-        console.log(playRound(playerSelection, computerSelection(3)));
-        console.log(playerScore, compScore);
-        if (i == 5) {
-            if (playerScore > compScore) {
-            console.log("Congratulations! Winner winner chicken dinner!");
-            } else if ((playerScore < compScore)) {
-                console.log("Imagine getting ratio'd by a program written by an alcoholic... couldn't be me")
-            } else {console.log("Holy smokes Lois, it's a tie!")};
-        }
+let roundLimit = 5;
+let i = 0;
+function playGame () {
+        i++
+    if (i === roundLimit) {
+        if (playerScore > compScore) {
+            container.removeAttribute('id')
+            const win = document.createElement("h1")
+            win.classList.add("win")
+            win.textContent = "Congratulations! Winner winner chicken dinner!"
+            container.appendChild(win)
+        } else if ((playerScore < compScore)) {
+            container.removeAttribute('id')
+            const loss = document.createElement("h1")
+            loss.classList.add("loss")
+            loss.textContent = "Imagine getting ratio'd by a program written by an alcoholic... couldn't be me"
+            container.appendChild(loss)
+        } else {container.removeAttribute('id')
+        const tie = document.createElement("h1")
+        tie.classList.add("tie")
+        tie.textContent = "Holy smokes Lois, it's a tie!"
+        container.appendChild(tie)};
     }
 }
 function playerWins () { 
@@ -64,19 +116,3 @@ function playerWins () {
 function compWins () {
     compScore++
 };
-//DOM Manipulators
-const container = document.querySelector('#container')
-const content = document.createElement("div");
-content.classList.add("content");
-const scores = document.createElement("h1")
-scores.classList.add("")
-scores.textContent = `${playerScore}, ${compScore}`
-const btnr = document.querySelector("#btnr")
-btnr.addEventListener('click', () => {
-    playRound("Rock")
-})
-
-
-
-
-	
